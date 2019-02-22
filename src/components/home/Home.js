962aa1973492e619d3908890320111f585316f89
Laura Ambrose <laura@kintohub.com>
2019-02-23 02:37:43 +0800

    Added some styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.sass'
 
 class Home extends Component {
   render() {
     const { isLoggedIn } = this.props
     return (
-      <div>
-        <h1>Hello</h1>
-        {isLoggedIn ? (
-          <p>Welcome to the madness</p>
-        ) : (
-          <p>You need to log in first to see the page</p>
-        )}
+      <div className={styles.HomePage}>
+        <div className={styles.background}>
+          <h1>Hello</h1>
+          {isLoggedIn ? (
+            <p>Welcome to the madness</p>
+          ) : (
+            <p>
+              You need to <Link to="/log-in">LOG IN</Link> or
+              <Link to="/sign-up">SIGN UP</Link> first to see the page
+            </p>
+          )}
+        </div>
       </div>
     )
   }
--- src/components/logIn/LogIn.js
+++ src/components/logIn/LogIn.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import LogInForm from '../forms/LogInForm'
+import { Link } from 'react-router-dom'
+
 class LogIn extends Component {
   render() {
     const { onSubmit } = this.props
     return (
       <div>
-        <h1>Sign Up</h1>
+        <h1>Log In</h1>
         <LogInForm onSubmit={onSubmit} />
+
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">Go to the sign in page</Link>
       </div>
     )
   }
